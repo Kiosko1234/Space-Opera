0f0e546ea849904ad3e9246da5ea2fac2c1dae85
Kiosko1234 <kio1414y@gmail.com>
2024-11-03 12:03:45 +0100

    Cutscene finished

--- Assets/Scripts/cutsceneManager.cs
+++ Assets/Scripts/cutsceneManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class cutsceneManager : MonoBehaviour
@@ -35,9 +36,18 @@ public class cutsceneManager : MonoBehaviour
         {
             panel.SetActive(true);
         }  
-        if(counter == 0)
+        if(counter == 0 || counter == 4 || counter == 6 || counter == 10 || counter == 13 || counter == 16 || counter == 19 || counter == 22)
         {
             headShot.texture = interComs;
+        }
+        if(counter == 3 || counter == 9 || counter == 12 || counter == 15 || counter == 17 || counter == 21)
+        {
+            headShot.texture = chep;
+        }
+        if(counter >= dialogue.Length)
+        {
+            faderImage.CrossFadeAlpha(1,1,true);
+            StartCoroutine(startUp());
         }
     }
 
@@ -47,6 +57,10 @@ public class cutsceneManager : MonoBehaviour
         if(counter == -1)
         {
             counter++;
+        }
+        if(counter >= dialogue.Length)
+        {
+            SceneManager.LoadScene(sceneName:"Tutorial");
         }
     }
     
