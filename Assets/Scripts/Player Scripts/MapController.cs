c3fd5310a4c893017d53b335bcee2b4d017c1625
Kiosko1234 <153931793+Kiosko1234@users.noreply.github.com>
2025-10-02 16:01:57 +0200

    wip the minimap autoscale with the level size

--- Assets/Scripts/Player Scripts/MapController.cs
+++ Assets/Scripts/Player Scripts/MapController.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class MapController : MonoBehaviour
 {
     public GameObject minmap;
+    Camera objektiv;
+    deathFog df; //150
     void Start()
     {
-   //     minmap = GameObject.FindGameObjectWithTag("Minimap");
+        //     minmap = GameObject.FindGameObjectWithTag("Minimap");
+        objektiv = this.gameObject.GetComponent<Camera>();
+        df = GameObject.FindGameObjectWithTag("Canvas").GetComponent<deathFog>();
     }
     // Update is called once per frame
     void Update()
